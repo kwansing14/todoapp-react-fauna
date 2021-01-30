f8762c1dbeb98dd25a2907deb8c8434a700cde18
kwansing14 <kwansing@hotmail.sg>
2021-01-30 14:25:07 +0800

    2nd commit

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,44 +1,38 @@
 import React from 'react'
 import { Form, Button, Input } from 'antd';
 import { createNote } from '../api'
+import { ToastContainer, toast } from 'react-toastify';
 
 const NoteForm = ({ form, notes, setNotes }) => {
 
-  const { getFieldDecorator, validateFields, resetFields } = form;
+  // const { validateFields, resetFields } = form;
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    validateFields((err, values) => {
-      if (!err && values.note) {
-        createNote(values.note).then(res => {
-          const newNotesArray = notes.concat([res])
-          setNotes(newNotesArray)
-          toast.success('Added Successfully')
-          resetFields()
-        })
-      }
-    });
+  const onFinish = (values) => {
+    // validateFields((err, values) => {
+		console.log('--',values)
+      
+		createNote(values).then(res => {
+			const newNotesArray = notes.concat([res])
+			setNotes(newNotesArray)
+			toast.success('Added Successfully')
+			// resetFields()
+		})
+      
   }
   
+  
   return (
-    <Form style={{marginBottom: '25px'}} layout="horizontal" onSubmit={handleSubmit}>
-      <Form.Item>
-        {getFieldDecorator('note', {
-        rules: [],
-        })(
+    <Form style={{marginBottom: '25px'}} layout="horizontal" onFinish={onFinish}>
+      <Form.Item name='note' rule={[]}>
           <Input
             className="note-input"
             size="large"
             placeholder="Add New Note"
-          />,
-          <Button>Create</Button>
-        )}
+          />
+          <Button type="primary" htmlType="submit">Create</Button>
       </Form.Item>
-
   </Form>
   )
 }
 
-const WrappedNoteForm = Form.create({name: 'notes_form'})(NoteForm)
-
-export default WrappedNoteForm;
+export default NoteForm;
